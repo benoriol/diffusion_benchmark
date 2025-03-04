65655baacc7a5486052f87b4050d5d4651c71c02
Benet <benet.oriol.sabat@gmail.com>
2025-03-04 12:45:26 -0800

    now setup forreal

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/devtools/repos/ubuntu$(source /etc/lsb-release; echo "$DISTRIB_RELEASE" | tr -d .)/$(dpkg --print-architecture)/ /"
+sudo apt install nsight-systems
+
+
+pip install -r requirements.txt
