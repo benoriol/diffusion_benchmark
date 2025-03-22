320219562999dd64cdd0d437000520be78e7d9a6
Benet Oriol <benet@cs.ucla.edu>
2025-03-22 11:52:23 -0700

    optional conda env in setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,28 @@
 ## If you want to use nsys profiling
-if [ "$1" = "--install-nsys" ]; then
+INSTALL_NSYS=false
+CREATE_ENV=false
+
+# Parse command line arguments
+for arg in "$@"; do
+    if [ "$arg" = "--install-nsys" ]; then
+        INSTALL_NSYS=true
+    fi
+    if [ "$arg" = "--create-env" ]; then
+        CREATE_ENV=true
+    fi
+done
+
+# Install nsys if requested
+if [ "$INSTALL_NSYS" = true ]; then
     sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
     sudo add-apt-repository "deb https://developer.download.nvidia.com/devtools/repos/ubuntu$(source /etc/lsb-release; echo "$DISTRIB_RELEASE" | tr -d .)/$(dpkg --print-architecture)/ /"
     sudo apt install nsight-systems
+fi
+
+# Create environment if requested
+if [ "$CREATE_ENV" = true ]; then
+    conda create -n diffbench python=3.11 -y
+    conda activate diffbench
 fi
 
 # Install this, maybe create a conda first
